16e64786247bde1c01baecc781fd11a156d32b86
JustinParviz <jarshravan@gmail.com>
2024-01-20 21:15:45 -0500

    Finished "take ticket" section and made improvements to "pay for parking" section

--- OOP_Parking_Garage.py
+++ OOP_Parking_Garage.py
@@ -9,12 +9,10 @@ class ParkingGarage():
     
     
     # ** TO DO **
-    # Take_Ticket functionality
-    #   Decrease the amount of tickets_available by 1 OR decrease the amount of parking_spaces available by 1
+    # 
+    #  
     #
-    # Pay_for_parking functionality
-    #   
-    
+   
     
     
     
@@ -23,15 +21,16 @@ class ParkingGarage():
     
     # Justin
     def take_ticket(self):
-        
+        print("Welcome to our Garage!")
         while True:
             if  self.parking_spaces_available >= 1:
-                print("Welcome to our Garage!")
-                self.user_response = input("Lucky for you, we currently have space available. Would you like a ticket? Yes or No ").title()
+                self.user_response = input("Lucky for you, we currently have space available. Would you like a ticket? Yes or No? ").title()
                 if self.user_response == "Yes":
                     print("Okay, here's your ticket")
                     self.total_tickets_available -= 1
                     print(f"There are {self.total_tickets_available} tickets left.")
+                    self.current_ticket["Ticket Paid"] = False
+                    print(self.current_ticket)
                     break
                 elif self.user_response == "No":
                     print("Okay have a nice day.")
@@ -43,22 +42,59 @@ class ParkingGarage():
                 break
 
 
+
+
+# pay_for_parking
+# - Display an input that waits for an amount from the user and store it in a variable
+# - If the payment variable is not empty then (meaning the ticket has been paid) -> display a message to the user that their ticket has been paid and they have 15mins to leave
+# - This should update the "current_ticket" dictionary key "paid" to True
+
+
+
+
+
     # Will
     def pay_for_parking(self):
-        if self.current_ticket in 
+        print("Thank you for using our parking garage today. Your total will be $20.00")
+        self.user_response = input("Please select a PAYMENT METHOD. Press 1 for CASH or 2 for CARD." )
+        if self.user_response == '1':
+            print("Please insert your CASH.")
+
+        elif self.user_response == '2':
+            print("Please swipe your CARD.")
+
+        else:
+            ("Sorry that is not a valid option. Please select CASH or CARD.")
+
+
+
+
         
-
-
-
-        if self.user_response == "Yes":
-            unpaid_ticket = input("Okay that'll be $20.00")
-            self.current_ticket += 1
+            # unpaid_ticket = input("Okay that'll be $20.00")
+            #     print("Thank you for your payment")
+            # self.current_ticket += 1
 
     
-    
+
+
+
+
+# -leave_garage
+# - If the ticket has been paid, display a message of "Thank You, have a nice day"
+# - If the ticket has not been paid, display an input prompt for payment
+# - Once paid, display message "Thank you, have a nice day!"
+# - Update parking_spaces list to increase by 1 (meaning add to the parkingSpaces list)
+# - Update tickets list to increase by 1 (meaning add to the tickets list)
+
+
+
+ 
      # Razvan
     def leave_garage(self):
         pass
+
+
+
 
 
 
